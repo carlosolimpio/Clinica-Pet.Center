935cfef9a682f8614494e9a4d04371ba91d40df7
Carlos Olimpio <Carlos@172.16.205.74>
2015-11-21 02:46:23 -0300

    Alterações em métodos e adição do metodo existeObjeto

--- Clinica.Pet-Center/src/clinica/pet_center/negocio/cadastros/CadastroAnimal.java
+++ Clinica.Pet-Center/src/clinica/pet_center/negocio/cadastros/CadastroAnimal.java
@@ -8,7 +8,7 @@ import clinica.pet_center.negocio.basicas.Animal;
 import clinica.pet_center.negocio.exceptions.IDIException;
 import clinica.pet_center.negocio.exceptions.OExistenteException;
 import clinica.pet_center.negocio.exceptions.ONExistenteException;
-import clinica.pet_center.utilidades.*;
+import clinica.pet_center.utilidades.Util;
 
 public class CadastroAnimal {
 	
@@ -18,46 +18,48 @@ public class CadastroAnimal {
 		repositorioAnimal = RepositorioAnimais.getInstancia();
 	}
 	
-	public void cadastraAnimal(Animal animal) throws OExistenteException, IDIException {
-		if(Util.isID(animal.getId()))
-			repositorioAnimal.insere(animal);
+	public void cadastrarAnimal(Animal animal) throws OExistenteException, IDIException, IllegalArgumentException {
+		if(Util.isID(animal.getId())) {
+			existeObjeto(animal);
+			repositorioAnimal.inserir(animal);
+		}
 		else
 			throw new IDIException(animal.getId());
 	}
 	
-	public Animal buscaAnimalId(String id) throws ONExistenteException, IDIException {
+	public Animal buscarAnimalId(String id) throws ONExistenteException, IDIException {
 		if(Util.isID(id))
-			return repositorioAnimal.busca(id);
+			return repositorioAnimal.buscar(id);
 		else
 			throw new IDIException(id);
 	}
 	
-	public ArrayList<Animal> listaAnimais() throws ONExistenteException
-	{
-		ArrayList<Animal> listaAnimal = repositorioAnimal.lista();
-		if(listaAnimal == null)
-		{
-			throw new ONExistenteException(Constantes.ANIMAL);
-		}
-		return listaAnimal;
+	public ArrayList<Animal> listarAnimais() throws IllegalArgumentException {
+		return repositorioAnimal.listar();
 	}
 	
-	public void removeAnimal(String id) throws ONExistenteException, IDIException {
+	public void removerAnimal(String id) throws ONExistenteException, IDIException {
 		if(Util.isID(id))
-			repositorioAnimal.remove(id);
+			repositorioAnimal.remover(id);
 		else 
 			throw new IDIException(id);
 	}
 	
-	public void alteraAnimal(String id, Animal novo) throws OExistenteException, ONExistenteException, IDIException {
-		if(novo != null && Util.isID(id))
-			repositorioAnimal.atualiza(novo, id);
+	public void alterarAnimal(String id, Animal novo) throws ONExistenteException, IDIException {
+		if(Util.isID(id))
+			repositorioAnimal.atualizar(novo, id);
 		else
 			throw new IDIException(id);
 	}
 	
-//	@Override
-//	public String toString(){
-//		return String.format("EXISTEM %d ANIMAIS NO SISTEMA", repositorioAnimal.lista().size());
-//	}
+	private void existeObjeto(Object obj) throws OExistenteException, IllegalArgumentException {
+		if(obj != null) {
+			for(Animal a : listarAnimais()) {
+				if(a.equals(obj))
+					throw new OExistenteException(obj);
+			}
+		}
+		else
+			throw new IllegalArgumentException("Objeto nulo");
+	}
 }
