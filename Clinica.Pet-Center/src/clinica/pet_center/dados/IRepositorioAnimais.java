8a68ae607a7cc65d01ba567cbc4705440f0651fc
Carlos Olimpio <Carlos@172.16.205.74>
2015-11-21 02:45:12 -0300

    Organização da fachada de acordo com alterações

--- Clinica.Pet-Center/src/clinica/pet_center/dados/IRepositorioAnimais.java
+++ Clinica.Pet-Center/src/clinica/pet_center/dados/IRepositorioAnimais.java
@@ -3,14 +3,13 @@ package clinica.pet_center.dados;
 import java.util.ArrayList;
 
 import clinica.pet_center.negocio.basicas.Animal;
-import clinica.pet_center.negocio.exceptions.OExistenteException;
 import clinica.pet_center.negocio.exceptions.ONExistenteException;
 
 public interface IRepositorioAnimais {
 	
-	void insere(Animal animal) throws OExistenteException;
-	Animal busca(String id) throws ONExistenteException;
-	ArrayList<Animal> lista() throws ONExistenteException;
-	void remove(String id) throws ONExistenteException;
-	void atualiza(Animal novo, String idVelho) throws OExistenteException, ONExistenteException;
+	void inserir(Animal animal);
+	Animal buscar(String id) throws ONExistenteException;
+	ArrayList<Animal> listar() throws IllegalArgumentException;
+	void remover(String id) throws ONExistenteException;
+	void atualizar(Animal novo, String idVelho) throws ONExistenteException;
 }
