353651eb2c4b70cb6d6049c8b0b66a79d46a7be6
Marcos Inacio de Paula Lima <marcos.inacio.recife@hotmail.com>
2015-12-03 23:00:44 -0300

    Funcionalidade em cadastrar e listar veterinario

--- Clinica.Pet-Center/src/clinica/petCenter/gui/PanelCadastrarVeterinario.java
+++ Clinica.Pet-Center/src/clinica/petCenter/gui/PanelCadastrarVeterinario.java
@@ -1,34 +1,54 @@
 package clinica.petCenter.gui;
 
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import clinica.petCenter.negocio.cadastros.FachadaCadastro;
+import clinica.petCenter.negocio.cadastros.IFachadaCadastro;
+import clinica.petCenter.negocio.classesBasicas.Veterinario;
+import clinica.petCenter.negocio.exceptions.OExistenteException;
+import clinica.petCenter.utilidades.Contadores;
+
+import javax.swing.JPasswordField;
 
 public class PanelCadastrarVeterinario extends JPanel {
 	
 	private JFrame frame;
 	
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
+	private JTextField tfDataNascimento;
+	private JTextField tfNome;
+	private JTextField tfCpf;
+	private JTextField tfEmail;
+	private JTextField tfCrmv;
 
+	private IFachadaCadastro fachada;
+	private JPasswordField pfSenha;
+	private JPasswordField pfCSenha;
+	private JTextField tfDataAdmissao;
 	/**
 	 * Create the panel.
 	 */
 	public PanelCadastrarVeterinario(JFrame frame) {
 		
 		setFrame(frame);
-		
+		setPreferredSize(new Dimension(600, 400));
 		setLayout(null);
+		
+		fachada = FachadaCadastro.getInstance();
+		
+		JLabel lblSubTitulo = new JLabel("Cadastro de Veterinario");
+		lblSubTitulo.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblSubTitulo.setBounds(29, 0, 269, 36);
+		add(lblSubTitulo);
 		
 		JLabel lblNome = new JLabel("Nome");
 		lblNome.setBounds(50, 50, 46, 14);
@@ -54,52 +74,60 @@ public class PanelCadastrarVeterinario extends JPanel {
 		lblSenha.setBounds(50, 250, 46, 14);
 		add(lblSenha);
 		
-		textField = new JTextField();
-		textField.setBounds(160, 127, 318, 20);
-		add(textField);
-		textField.setColumns(10);
+		tfDataNascimento = new JTextField();
+		tfDataNascimento.setBounds(160, 127, 318, 20);
+		add(tfDataNascimento);
+		tfDataNascimento.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(160, 47, 318, 20);
-		add(textField_1);
+		tfNome = new JTextField();
+		tfNome.setColumns(10);
+		tfNome.setBounds(160, 47, 318, 20);
+		add(tfNome);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(160, 87, 318, 20);
-		add(textField_2);
+		tfCpf = new JTextField();
+		tfCpf.setColumns(10);
+		tfCpf.setBounds(160, 87, 318, 20);
+		add(tfCpf);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(160, 167, 318, 20);
-		add(textField_3);
+		tfEmail = new JTextField();
+		tfEmail.setColumns(10);
+		tfEmail.setBounds(160, 167, 318, 20);
+		add(tfEmail);
 		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		textField_4.setBounds(160, 207, 318, 20);
-		add(textField_4);
-		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(160, 247, 318, 20);
-		add(textField_5);
+		tfCrmv = new JTextField();
+		tfCrmv.setColumns(10);
+		tfCrmv.setBounds(160, 207, 318, 20);
+		add(tfCrmv);
 		
 		JLabel lblConfirmarSenha = new JLabel("Confirmar Senha");
-		lblConfirmarSenha.setBounds(50, 293, 100, 14);
+		lblConfirmarSenha.setBounds(50, 290, 100, 14);
 		add(lblConfirmarSenha);
-		
-		textField_6 = new JTextField();
-		textField_6.setColumns(10);
-		textField_6.setBounds(160, 290, 318, 20);
-		add(textField_6);
 		
 		JButton btnCadastrar = new JButton("Cadastrar");
 		btnCadastrar.setBounds(501, 366, 89, 23);
 		add(btnCadastrar);
+		btnCadastrar.addActionListener(new EvntBtnCadastrar());
 		
 		JButton btnVoltarPanelAdm = new JButton("Voltar");
 		btnVoltarPanelAdm.setBounds(402, 366, 89, 23);
 		add(btnVoltarPanelAdm);
+		
+		pfSenha = new JPasswordField();
+		pfSenha.setBounds(160, 247, 318, 20);
+		add(pfSenha);
+		
+		pfCSenha = new JPasswordField();
+		pfCSenha.setBounds(160, 287, 318, 20);
+		add(pfCSenha);
+		
+		JLabel lblDataAdmissao = new JLabel("Data Admissao");
+		lblDataAdmissao.setBounds(50, 330, 100, 14);
+		add(lblDataAdmissao);
+		
+		tfDataAdmissao = new JTextField();
+		tfDataAdmissao.setColumns(10);
+		tfDataAdmissao.setBounds(160, 327, 318, 20);
+		add(tfDataAdmissao);
 		EvntBtnVoltarPanelAdm evntVoltarPanelAdm = new EvntBtnVoltarPanelAdm();
 		btnVoltarPanelAdm.addActionListener(evntVoltarPanelAdm);
 
@@ -107,6 +135,28 @@ public class PanelCadastrarVeterinario extends JPanel {
 	
 	private void setFrame(JFrame frame){
 		this.frame = frame;
+	}
+	
+	private class EvntBtnCadastrar implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			String senha = new String(pfSenha.getPassword());
+			String nome = tfNome.getText();
+			String cpf = tfCpf.getText();
+			String dataNascimento = tfDataNascimento.getText();
+			String email = tfEmail.getText();
+			String crmv = tfCrmv.getText();
+			String dataAdmissao = tfDataAdmissao.getText();
+			
+			Veterinario vet = new Veterinario(nome, cpf, dataNascimento, email, dataAdmissao, crmv, senha);
+			
+			try {
+				fachada.cadastrarFuncionario(vet);
+				JOptionPane.showMessageDialog(null, vet.getId(), "Cadastro", JOptionPane.PLAIN_MESSAGE);
+			} catch(OExistenteException oee) {
+				JOptionPane.showMessageDialog(null, "Funcionario já cadastrado no sistema.", "Erro", JOptionPane.CANCEL_OPTION);
+				Contadores.decrementaPessoas();
+			}
+		}
 	}
 	
 	private class EvntBtnVoltarPanelAdm implements ActionListener {
@@ -118,5 +168,4 @@ public class PanelCadastrarVeterinario extends JPanel {
 			
 		}
 	}
-
 }
--- Clinica.Pet-Center/src/clinica/petCenter/gui/PanelListarVeterinario.java
+++ Clinica.Pet-Center/src/clinica/petCenter/gui/PanelListarVeterinario.java
@@ -3,6 +3,8 @@ package clinica.petCenter.gui;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,16 +12,23 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import clinica.petCenter.negocio.cadastros.FachadaCadastro;
+import clinica.petCenter.negocio.cadastros.IFachadaCadastro;
+import clinica.petCenter.negocio.classesBasicas.Funcionario;
+import clinica.petCenter.negocio.classesBasicas.Veterinario;
+
 public class PanelListarVeterinario extends JPanel {
 
 private JFrame frame;
 	
 	
-	private JTextField tfCpfOperador;
-	private JTextField tfNomeOperador;
-	private JTextField tfIdOperador;
-	private JTextField tfListarOperador;
-	private JTextField textField;
+	private JTextField tfCpfVet;
+	private JTextField tfNomeVet;
+	private JTextField tfIdVet;
+	private JTextField tfListarVet;
+	private JTextField tfCrmv;
+	
+	private IFachadaCadastro fachada;
 
 	/**
 	 * Create the panel.
@@ -30,38 +39,50 @@ private JFrame frame;
 		
 		setFrame(frame);
 		
+		fachada = FachadaCadastro.getInstance();
+		
 		JLabel lblNome = new JLabel("Nome");
 		lblNome.setBounds(20, 23, 46, 14);
 		add(lblNome);
 		
-		tfNomeOperador = new JTextField();
-		tfNomeOperador.setColumns(10);
-		tfNomeOperador.setBounds(66, 20, 210, 20);
-		add(tfNomeOperador);
+		tfNomeVet = new JTextField();
+		tfNomeVet.setColumns(10);
+		tfNomeVet.setBounds(66, 20, 210, 20);
+		add(tfNomeVet);
 		
 		JLabel lblCpf = new JLabel("CPF");
 		lblCpf.setBounds(20, 56, 46, 14);
 		add(lblCpf);
 		
-		tfCpfOperador = new JTextField();
-		tfCpfOperador.setBounds(66, 53, 210, 20);
-		add(tfCpfOperador);
-		tfCpfOperador.setColumns(10);
+		tfCpfVet = new JTextField();
+		tfCpfVet.setBounds(66, 53, 210, 20);
+		add(tfCpfVet);
+		tfCpfVet.setColumns(10);
 		
 		JLabel lblIdVeterinario = new JLabel("Id Veterin\u00E1rio");
 		lblIdVeterinario.setBounds(286, 23, 86, 14);
 		add(lblIdVeterinario);
 		
-		tfIdOperador = new JTextField();
-		tfIdOperador.setColumns(10);
-		tfIdOperador.setBounds(382, 20, 192, 20);
-		add(tfIdOperador);
+		tfIdVet = new JTextField();
+		tfIdVet.setColumns(10);
+		tfIdVet.setBounds(382, 20, 192, 20);
+		add(tfIdVet);
 		
 		JButton btnListar = new JButton("Listar");
+		btnListar.addActionListener(new EvntBtnListar());
 		btnListar.setBounds(485, 85, 89, 23);
 		add(btnListar);
 		
 		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tfCpfVet.setText("");
+				tfCrmv.setText("");
+				tfIdVet.setText("");
+				tfListarVet.setText("");
+				tfNomeVet.setText("");
+			}
+		});
 		btnLimpar.setBounds(382, 85, 89, 23);
 		add(btnLimpar);		
 		
@@ -83,15 +104,15 @@ private JFrame frame;
 		btnAtualizar.setBounds(20, 365, 93, 23);
 		add(btnAtualizar);
 		
-		tfListarOperador = new JTextField();
-		tfListarOperador.setBounds(21, 119, 553, 235);
-		add(tfListarOperador);
-		tfListarOperador.setColumns(10);
+		tfListarVet = new JTextField();
+		tfListarVet.setBounds(21, 119, 553, 235);
+		add(tfListarVet);
+		tfListarVet.setColumns(10);
 		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(382, 53, 192, 20);
-		add(textField);
+		tfCrmv = new JTextField();
+		tfCrmv.setColumns(10);
+		tfCrmv.setBounds(382, 53, 192, 20);
+		add(tfCrmv);
 		
 		JLabel lblCrmv = new JLabel("CRMV");
 		lblCrmv.setBounds(326, 56, 46, 14);
@@ -100,6 +121,21 @@ private JFrame frame;
 	
 	private void setFrame(JFrame frame){
 		this.frame = frame;
+	}
+	
+	private class EvntBtnListar implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			try {
+				List<Veterinario> arrayVT = fachada.listarVeterinarios();
+				if(arrayVT != null)
+					for(Funcionario fx : arrayVT) 
+					{
+						System.out.println(fx.toString());
+					}
+			} catch (Exception e2) {
+				e2.getMessage();
+			}
+		}
 	}
 	
 	private class EvntBtnVoltarPanelAdm implements ActionListener {
